2addff620f67a3dd13cf9d8e9cf19fec5e8a2ae8
Ibrahim S Barry <isbarry@Ibrahims-MBP.lan>
2021-11-19 18:49:39 -0500

    finish

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -55,7 +55,7 @@ export function getStatistics(array) {
         median: getMedian(array),
         min: Math.min(...array), 
         max: Math.max(...array),
-        variance: Math.variance(array),
+        variance: variance(array, array.reduce((a, b) => a + b) / array.length),
         standard_deviation: Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / array.length)
         
 
